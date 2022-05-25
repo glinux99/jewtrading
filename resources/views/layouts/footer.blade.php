17dd90c47e6d4360e5bc002e1c26f051875f1737
glinux99 <interlab21@yahoo.com>
2022-05-25 05:37:10 +0200

    jewtrading

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -10,8 +10,8 @@
             <div class="col-4 px-4 bg-danger d-flex justify-content-center align-items-center">
                 <input type="text" name="" id="" class="form-control" placeholder="Votre Email adresse">
             </div>
-            <div class="col-4 bg-warning  d-flex justify-content-start align-items-center">
-                <button class="btn text-white btn-jew"><span>Abonnez-vous</span><span class="bi-chevron-right"></span></button>
+            <div class="col-4 bg-dark d-flex justify-content-start align-items-center ps-5">
+                <button class="btn text-white btn-jew ms-5"><span>Abonnez-vous</span><span class="bi-chevron-right"></span></button>
             </div>
         </div>
     </form>
@@ -44,16 +44,16 @@
                         {{__("LES GARANTIES")}}
                     </h5>
                     <p>
-                        <h3>
-                            Nos servies sont (ont):
-                        </h3>
-                        <ul class="list-unstyled">
-                            <li class="border-bottom border-2 p-2"> <span class="bi-chevron-right"></span> Rapide et facile</li>
-                            <li class="border-bottom border-2 p-2"> <span class="bi-chevron-right"></span> Une inspection Transparente</li>
-                            <li class="border-bottom border-2 p-2"> <span class="bi-chevron-right"></span> Offert immédiatement</li>
-                            <li class="border-bottom border-2 p-2"> <span class="bi-chevron-right"></span> Formalités administratives sans tracas</li>
-                            <li class="border-bottom border-2 p-2"> <span class="bi-chevron-right"></span> Transactions de paiement sécurisées</li>
-                        </ul>
+                    <h6>
+                        Nos servies sont (ont):
+                    </h6>
+                    <ul class="list-unstyled">
+                        <li class="border-bottom border-2 p-2 h6"> <span class="bi-chevron-right"></span> Rapide et facile</li>
+                        <li class="border-bottom border-2 p-2 h6"> <span class="bi-chevron-right"></span> Une inspection Transparente</li>
+                        <li class="border-bottom border-2 p-2 h6"> <span class="bi-chevron-right"></span> Offert immédiatement</li>
+                        <li class="border-bottom border-2 p-2 h6"> <span class="bi-chevron-right"></span> Formalités administratives sans tracas</li>
+                        <li class="border-bottom border-2 p-2 h6"> <span class="bi-chevron-right"></span> Transactions de paiement sécurisées</li>
+                    </ul>
                     </p>
                 </div>
                 <div class="col-md-4">
@@ -61,42 +61,42 @@
                         {{__("CONTACT US")}}
                     </h5>
                     <p>
-                        <ul class="list-unstyled">
-                            <li class="d-flex">
-                                <span class="bi-person-lines-fill text-white bi--xl"></span>
-                                <div class="ms-2">
-                                    <h6 class="fw-bold p-0 m-0">
-                                        {{ __("Adresse") }}
-                                    </h6>
-                                    <p class="text-muted small">
-                                        {{__("Com.De Goma,Q. Les volcans, Av. du Touriste, N-2")}}
-                                    </p>
-                                </div>
-                            </li>
-                            <li class="d-flex">
-                                <span class="bi-telephone-inbound-fill text-white bi--xl"></span>
-                                <div class="ms-2">
-                                    <h6 class="fw-bold p-0 m-0">
-                                        {{ __("Phone") }}
-                                    </h6>
-                                    <p class="text-muted small">
-                                        <a href="tel:+243814536299">+243 814 536 299</a>
-                                        <a href="https://wa.me/+243997721070">+243 997 721 070</a>
-                                    </p>
-                                </div>
-                            </li>
-                            <li class="d-flex">
-                                <span class="bi-envelope text-white bi--xl"></span>
-                                <div class="ms-2">
-                                    <h6 class="fw-bold p-0 m-0">
-                                        {{ __("Email") }}
-                                    </h6>
-                                    <p class="text-muted small">
-                                        <a href="mailto:kwiratuwe@gmail.com">kwiratuwe@gmail.com</a>
-                                    </p>
-                                </div>
-                            </li>
-                        </ul>
+                    <ul class="list-unstyled">
+                        <li class="d-flex">
+                            <span class="bi-person-lines-fill text-white bi--xl"></span>
+                            <div class="ms-2">
+                                <h6 class="fw-bold p-0 m-0">
+                                    {{ __("Adresse") }}
+                                </h6>
+                                <p class="text-muted small">
+                                    {{__("Com.De Goma,Q. Les volcans, Av. du Touriste, N-2")}}
+                                </p>
+                            </div>
+                        </li>
+                        <li class="d-flex">
+                            <span class="bi-telephone-inbound-fill text-white bi--xl"></span>
+                            <div class="ms-2">
+                                <h6 class="fw-bold p-0 m-0">
+                                    {{ __("Phone") }}
+                                </h6>
+                                <p class="text-muted small">
+                                    <a href="tel:+243814536299">+243 814 536 299</a>
+                                    <a href="https://wa.me/+243997721070">+243 997 721 070</a>
+                                </p>
+                            </div>
+                        </li>
+                        <li class="d-flex">
+                            <span class="bi-envelope text-white bi--xl"></span>
+                            <div class="ms-2">
+                                <h6 class="fw-bold p-0 m-0">
+                                    {{ __("Email") }}
+                                </h6>
+                                <p class="text-muted small">
+                                    <a href="mailto:kwiratuwe@gmail.com">kwiratuwe@gmail.com</a>
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
                     </p>
                 </div>
             </div>
