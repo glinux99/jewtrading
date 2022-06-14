52e84a59da4c1bf89432a822a35507b410b59805
glinux99 <interlab21@yahoo.com>
2022-06-14 14:56:39 +0200

    jewtrading

--- resources/views/layouts/error.blade.php
+++ resources/views/layouts/error.blade.php
@@ -1,18 +1,18 @@
 <div>
     @if(session('error')==='no_error')
-    <div class="mt-2 alert alert-dismissible fade show bg-success col-lg-5 mx-auto" role="alert">
-        <strong>{{ __("Success!") }}</strong>
+    <div class="mt-2 alert alert-dismissible fade show bg-danger col-lg-5 mx-auto shadow" role="alert">
+        <strong class="text-primary"><span class=" bi-check-all"></span>{{ __("Success!") }}</strong>
         {{__("Traitement effectué avec success")}} <br>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @elseif(session('error')==='one_thing_not_running')
-    <div class="mt-2 alert alert-dismissible fade show bg-danger col-lg-5 mx-auto" role="alert">
+    <div class="mt-2 alert alert-dismissible fade show bg-danger col-lg-5 mx-auto shadow" role="alert">
         <strong>{{ __("ERREUR!") }}</strong>
         {{__("Nous ne pouvons pas satisfaire à votre requête car plusieurs erreurs ont été detectées")}} <br>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
     @elseif(session('error')==='no_autorization')
-    <div class="mt-2 alert alert-dismissible fade show bg-danger col-lg-5 mx-auto" role="alert">
+    <div class="mt-2 alert alert-dismissible fade show bg-danger col-lg-5 mx-auto shadow" role="alert">
         <strong>{{ __("ERREUR!") }}</strong>
         {{__("Vous n'avez pas d'autorisation pour acceder à ce service ou effectuer cette opération!!!")}} <br>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
