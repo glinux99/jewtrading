28aa5dcb004b6a282d827f9bec362d8b88b488bb
glinux99 <interlab21@yahoo.com>
2023-01-16 05:36:52 -0500

    first commit

--- app/Http/Controllers/adminjews/AdminController.php
+++ app/Http/Controllers/adminjews/AdminController.php
@@ -66,14 +66,14 @@ class AdminController extends Controller
                 // $path = Image::make($image)->encode('jpg', 65)
                 //     ->save(public_path('/storage/images/galerie/' . $fileName));
 
-                // $path = $image->storeAs(
-                //     'images/galerie',
-                //     $fileName,
-                //     'public'
-                // );
+                $path = $image->storeAs(
+                    'images/galerie',
+                    $fileName,
+                    'public'
+                );
                 // dd($path);
-                $resize_image = Image::make($image)->resize(300, 300)->encode('png');
-                $resize_image->save(public_path('/storage/images/galerie/') . $fileName, 100);
+                // $resize_image = Image::make($image)->resize(300, 300)->encode('png');
+                // $resize_image->save(public_path('/storage/images/galerie/') . $fileName, 100);
                 // $team->profile_picture = $path;
 
 
